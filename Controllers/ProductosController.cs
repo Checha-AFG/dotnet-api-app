14eae529d522a5d77430154f0c4c5fc630027ff9
pscherios <99372151+pscherios@users.noreply.github.com>
2022-11-06 08:13:41 -0600

    Comentarios
    agregada descripcion para la funcionalidad de cada get

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -8,10 +8,12 @@ namespace API_APP.Controllers{
         public ProductosController(){
             dbConexion = Conectar.Create();
         }
+        //este get devuelve todos los valores
         [HttpGet]
         public ActionResult Get(){
             return Ok(dbConexion.Productos.ToArray());
         }
+        //este get devuelve el valor del id en la direccion api/productos/7 (como ejemplo)
         [HttpGet("{id}")]
         public ActionResult GetAction(int id){
             var productos = dbConexion.Productos.SingleOrDefault(a=>a.idproducto==id);
